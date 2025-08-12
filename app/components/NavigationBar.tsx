2c02557c73555c061a928ab170d7805c7fc4ba01
ViktorU <ushakov777rus@gmail.com>
2025-08-12 18:07:11 +0300

    добавил лого

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -8,19 +8,31 @@ import { UserBadge } from "@/app/components/UserBadge";
 
 import "./NavigationBar.css";
 
+import Image from "next/image";
+
 export function NavigationBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-      <header className="header">
-        <div className="logo">ХЗ</div>
-        <nav className="nav">
-          <Link href="/">Главная</Link>
-          <Link href="/tournaments">Турниры</Link>
-          <Link href="/rating">Рейтинг</Link>
-          <Link href="/matches">Матчи</Link>
-        </nav>
-        <UserBadge />
-      </header>
+    <header className="header">
+      <div className="logo">
+        <Link href="/">
+          <Image
+            src="/logo.png" // положи картинку в public/logo.png
+            alt="Логотип"
+            width={40} // размеры подгони под макет
+            height={40}
+            priority
+          />
+        </Link>
+      </div>
+      <nav className="nav">
+        <Link href="/">Главная</Link>
+        <Link href="/tournaments">Турниры</Link>
+        <Link href="/rating">Рейтинг</Link>
+        <Link href="/matches">Матчи</Link>
+      </nav>
+      <UserBadge />
+    </header>
   );
 }
