14b566f215a1225836c644b87509c13ba32eef61
ViktorU <ushakov777rus@gmail.com>
2025-08-17 08:04:53 +0300

    Добавил YM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,18 @@
 import "./globals.css";
-import Script from "next/script";
 import { UserProvider } from "./components/UserContext";
-import { YandexMetrikaHit } from "./ym"; // добавим ниже
-
-const YM_ID = process.env.NEXT_PUBLIC_YM_ID;
+import { YandexMetrika } from "./components/YandexMetrica";
+import { YandexMetrikaHit } from "./ym";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-
-  console.log("process.env.NODE_ENV",process.env.NODE_ENV, YM_ID);
   return (
     <html lang="ru">
       <body>
         <UserProvider>{children}</UserProvider>
-
-        {/* Яндекс.Метрика (только в проде и если задан ID) */}
-        {YM_ID && (
-          <>
-            <Script
-              id="ym-loader"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                  k.async=1;k.src=r;a.parentNode.insertBefore(k,a)})
-                  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                  ym(${YM_ID}, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                  });
-                `,
-              }}
-            />
-            {/* SPA-хиты при клиентских переходах */}
-            <YandexMetrikaHit />
-
-            {/* noscript-пиксель (на случай отключённого JS) */}
-            <noscript>
-              <div>
-                <img src={`https://mc.yandex.ru/watch/${YM_ID}`} style={{ position: "absolute", left: "-9999px" }} alt="" />
-              </div>
-            </noscript>
-          </>
-        )}
+        <YandexMetrika />
+        <YandexMetrikaHit />
+        <noscript>
+          <div><img src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_YM_ID}`} style={{ position:"absolute", left:"-9999px" }} alt=""/></div>
+        </noscript>
       </body>
     </html>
   );
