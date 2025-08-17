6a729760b0b2593b38e1047ac4191f47b4510da9
ViktorU <ushakov777rus@gmail.com>
2025-08-17 08:06:49 +0300

    Добавил YM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,11 @@
 import "./globals.css";
 import { UserProvider } from "./components/UserContext";
-import { YandexMetrika } from "./components/YandexMetrica";
-import { YandexMetrikaHit } from "./ym";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
       <body>
         <UserProvider>{children}</UserProvider>
-        <YandexMetrika />
-        <YandexMetrikaHit />
-        <noscript>
-          <div><img src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_YM_ID}`} style={{ position:"absolute", left:"-9999px" }} alt=""/></div>
-        </noscript>
       </body>
     </html>
   );
