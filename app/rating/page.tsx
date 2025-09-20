ca3311522030ec4b8841f4a76df91d67d0b4b153
ViktorU <ushakov777rus@gmail.com>
2025-09-20 19:42:03 +0300

    Рейтинг игроков

--- app/rating/page.tsx
+++ app/rating/page.tsx
@@ -1,9 +1,13 @@
 // server component (по умолчанию)
 
+import { Suspense } from "react";
+
 import { PlayerListView } from "./PlayerListView";
 
 export default function RatingPage() {
   return (
-    <PlayerListView clubId={null}/>
+    <Suspense fallback={null}>
+      <PlayerListView clubId={null} />
+    </Suspense>
   );
 }
