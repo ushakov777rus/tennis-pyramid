d1d4da60e86e38696048cf37049f36fdfd76ffbc
ViktorU <ushakov777rus@gmail.com>
2025-10-12 15:10:56 +0300

    Права на редактирования турнира

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://honeycup.ru/:path*',
-        permanent: true
-      }
-    ];
+    // Для продакшена редиректим все кроме localhost
+    if (process.env.NODE_ENV === 'production' && process.env.VERCEL_ENV === 'production') {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://honeycup.ru/:path*',
+          permanent: true,
+          has: [
+            {
+              type: 'host',
+              value: 'tennis-pyramid\.vercel\.app',
+            }
+          ]
+        }
+      ];
+    }
+    
+    // Для разработки - никаких редиректов
+    return [];
   }
 };
 
